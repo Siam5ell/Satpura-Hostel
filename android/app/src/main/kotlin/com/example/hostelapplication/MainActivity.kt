02392d2f60438cf408b67180f024f81e329ec69d
Siam5ell <jishnusingh1304@gmail.com>
2024-02-27 14:41:08 +0530

    Mergin into main

--- android/app/src/main/kotlin/com/example/hostelapplication/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.satpura.hostel
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/logic/modules/user_model.dart
+++
@@ -1,9 +0,0 @@
-class FireBaseUser {
-  final String uid;
-  final String? email;
-
-  FireBaseUser({
-    required this.uid,
-    this.email,
-  });
-}
